cf80c05ddb41b757097b040d07b054cb1c0ea8b7
KayCeM <kgomotsocomfortmaduna@outlook.com>
2024-03-22 05:08:57 -0700

    update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for count in range(1, 101):
     if count in range(15, 101, 15):
-        print("FizzBuzz ", end=" ")
+        print("FizzBuzz" .format(count), end=" ")
     if count in range(3, 101, 3):
-        print("Fizz ", end=" ")
+        print("Fizz " .format(count), end=" ")
     elif count in range(5, 101, 5):
-        print("Buzz ", end=" ")
+        print("Buzz " .format(count), end=" ")
     else:
         print(count, end=" ")
